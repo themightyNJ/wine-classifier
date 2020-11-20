36c588de11cdb2073a947d2ef8bfc5c6ecc4fc6a
Nitish Jha <jhanitish.2298@gmail.com>
2020-11-20 17:18:37 +0530

    model script

---
+++ model.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+from sklearn.datasets import load_wine
+wine = load_wine()
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+classifier = svm.SVC(kernel = "linear")
+import pickle
+
+features = pd.DataFrame(data=wine["data"],columns=wine["feature_names"])
+
+features["target"] = wine["target"]
+features['class'] = features['target'].map(lambda ind: wine['target_names'][ind])
+
+data_train, data_test, label_train, label_test = train_test_split(wine['data'],wine['target'],test_size=0.25)
+
+classifier.fit(data_train,label_train)
+
+pickle.dump(classifier, open('model.pkl','wb'))
