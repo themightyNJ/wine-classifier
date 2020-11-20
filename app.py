82f228e0007a26c6394bfbb1042150547cae2abb
Nitish Jha <jhanitish.2298@gmail.com>
2020-11-20 19:45:24 +0530

    Created web app

---
+++ app.py
@@ -0,0 +1,27 @@
+import numpy as np
+from flask import Flask, request, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    features = [float(x) for x in request.form.values()]
+    final_features = [np.array(features)]
+    prediction = model.predict(final_features)
+
+    output = prediction[0]
+    output_ = {0:"A",1:"B",2:"C"}
+
+    return render_template('index.html', prediction_text='Wine belongs to class: {}'.format(output_[output]))
+
+if __name__ == "__main__":
+    app.run(debug=True)
