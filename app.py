6408c3f8c13cea0ad41ea4d96de380f0a60ca81c
Nitish Jha <47714321+themightyNJ@users.noreply.github.com>
2020-11-21 11:35:32 +0530

    Turned Debugging off

--- app.py
+++ app.py
@@ -24,4 +24,4 @@ def predict():
     return render_template('index.html', prediction_text='Wine belongs to class: {}'.format(output_[output]))
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run()
